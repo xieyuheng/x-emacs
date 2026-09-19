9359c1d226571b62ecfe29d00134ee2d5414b3a0
Xie Yuheng <xyheme@gmail.com>
2026-09-19 13:32:47 +0800

    up

--- src/langs/config-web.el
+++ src/langs/config-web.el
@@ -7,6 +7,7 @@
 (add-to-list 'auto-mode-alist '("\\.svelte\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mimor\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 
 (define-key web-mode-map (kbd "C-c C-c") 'web-mode-tag-match)
 
